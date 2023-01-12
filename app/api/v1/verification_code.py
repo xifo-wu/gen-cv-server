2243797e77c5b9c351082c15b3494cb2e74f60b6
Xifo <xifo.wu@gmail.com>
2023-01-12 14:36:05 +0800

    feat: 新增 DEV 时不发送验证码

--- app/api/v1/verification_code.py
+++ app/api/v1/verification_code.py
@@ -1,3 +1,4 @@
+import os
 from flask import render_template
 from flask_mail import Message
 from app.api.v1 import api_v1
@@ -19,12 +20,18 @@ def verification_code():
     if redis_client.exists(result['key']) == 1:
         return format(message="验证码还在有效时间内")
 
-    if result['purpose'] == 'email_register':
-        # TODO 拉黑临时邮箱功能
-        msg = Message(recipients=[result['data']])
-        msg.subject = "您有新的验证码，请查收"
-        msg.html = render_template("otp.html", code=code)
-        mail.send(msg)
+    # 产品环境时在发送到具体的用户上。
+    if os.getenv('FLASK_CONFIG') == 'production':
+        if result['purpose'] == 'email_register':
+            # TODO 拉黑临时邮箱功能
+            msg = Message(recipients=[result['data']])
+            msg.subject = "您有新的验证码，请查收"
+            msg.html = render_template("otp.html", code=code)
+            mail.send(msg)
+
+    if os.getenv('FLASK_CONFIG') == 'development':
+        # TODO 使用 LOG 打印
+        print('verification_code: ', code)
 
     redis_client.setex(result['key'], 15 * 60, code)
 
