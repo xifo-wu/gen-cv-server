ef4060d9567782c3bed788e4f6e42990aa8ce3d2
Xifo <xifo.wu@gmail.com>
2023-01-12 10:27:43 +0800

    feat(base): 新增退出登录接口

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from lib import response
 from app.models import User
 from app.schemas.user import UserSchema
-from app.extensions import jwt, db
+from app.extensions import jwt, db, redis_client
 from app.api.v1 import api_v1
 
 api = Blueprint('api', __name__)
@@ -35,3 +35,16 @@ def unauthorized_callback(cause):
 def user_lookup_callback(jwt_header, jwt_payload):
     user = db.session.scalar(db.select(User).filter_by(id=jwt_payload['sub']))
     return UserSchema().dump(user)
+
+
+@jwt.revoked_token_loader
+def revoked_token_callback(jwt_header, jwt_payload):
+    return response.error(message="登录已过期", status_code=401)
+
+
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload: dict):
+    # 检查redis块列表中是否存在JWT的回调函数
+    jti = jwt_payload["jti"]
+    token_in_redis = redis_client.get(jti)
+    return token_in_redis is not None
--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,7 +1,8 @@
-from flask_jwt_extended import get_jwt_identity, create_access_token, create_refresh_token, jwt_required, get_current_user
+import time
+from flask_jwt_extended import get_jwt, get_jwt_identity, create_access_token, create_refresh_token, jwt_required, get_current_user
 
 from app.api.v1 import api_v1
-from app.extensions import db
+from app.extensions import db, redis_client
 from app.models.user import User
 from app.schemas.auth import PasswordLoginSchema, RegisterSchema
 from app.schemas.user import UserSchema
@@ -54,6 +55,15 @@ def login():
     meta['refresh_token'] = create_refresh_token(identity=result['id'])
 
     return response.format(data=result, meta=meta)
+
+
+@api_v1.delete('/logout')
+@jwt_required()
+def logout():
+    jwt = get_jwt()
+    exp = jwt['exp'] - int(time.time())
+    redis_client.set(jwt['jti'], "", ex=exp)
+    return response.format(message="退出登录成功")
 
 
 @api_v1.get('/user')
