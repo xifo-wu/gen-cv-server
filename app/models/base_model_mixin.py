4c3317b4f74a10993e2ec91b74ed3f9cb20125c4
Xifo <xifo.wu@gmail.com>
2023-01-17 15:59:00 +0800

    feat(base): BaseModelMixin updated_at 添加默认时间

--- app/models/base_model_mixin.py
+++ app/models/base_model_mixin.py
@@ -6,4 +6,4 @@ class BaseModelMixin:
     time_now = datetime.utcnow
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, nullable=False, default=time_now)
-    updated_at = db.Column(db.DateTime, onupdate=time_now)
+    updated_at = db.Column(db.DateTime, onupdate=time_now, default=time_now)
